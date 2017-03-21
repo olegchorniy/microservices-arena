ca44e691619431c48715bad9a5114c3a4833a839
Oleg <alig007@mail.ru>
2017-03-22 00:38:58 +0200

    amqp test module added

---
+++ amqp/src/main/java/my/example/ampq/AmpqApplication.java
@@ -0,0 +1,12 @@
+package my.example.ampq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmpqApplication.class, args);
+    }
+}
