ae884bc3e99e1bdaddc5b6055ba56d5f4dcb675e
Oleg <alig007@mail.ru>
2017-04-02 11:47:57 +0300

    Actuator added.

--- cassandra-demo/src/main/java/my/example/cassandra/CassandraDemoApplication.java
+++ cassandra-demo/src/main/java/my/example/cassandra/CassandraDemoApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /* Don't forget to uncomment exclusions for Cassandra auto-configuration classes with this option. */
-@SpringBootApplication(scanBasePackages = "my.example.cassandra.nativeapi")
-//@SpringBootApplication(scanBasePackages = "my.example.cassandra.spring")
+//@SpringBootApplication(scanBasePackages = "my.example.cassandra.nativeapi")
+@SpringBootApplication(scanBasePackages = "my.example.cassandra.spring")
 public class CassandraDemoApplication {
 
     public static void main(String[] args) {
